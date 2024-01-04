2f36adda68026ba699cb193ca91570c103e1d97d
dohungyi <dohungyi>
2024-01-04 13:31:11 +0700

    feat: base api service identity

--- src/Services/Identity/Identity.Api/Extensions/ApplicationExtensions.cs
+++ src/Services/Identity/Identity.Api/Extensions/ApplicationExtensions.cs
@@ -1,6 +1,15 @@
 ﻿namespace Identity.Api.Extensions;
 
-public class ApplicationExtensions
+public static class ApplicationExtensions
 {
-    
+    public static void UseSwaggerVersioning(this IApplicationBuilder app)
+    {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.Api version one");
+            c.SwaggerEndpoint("/swagger/v2/swagger.json", "Catalog.Api version two");
+        });
+    }
 }
--- src/Services/Identity/Identity.Api/Extensions/HostExtensions.cs
+++ src/Services/Identity/Identity.Api/Extensions/HostExtensions.cs
@@ -1,6 +1,19 @@
-﻿namespace Identity.Api.Extensions;
+﻿using Serilog;
+using SharedKernel.Configure;
 
-public class HostExtensions
+namespace Identity.Api.Extensions;
+
+public static class HostExtensions
 {
+    public static void AddAppConfigurations(this ConfigureHostBuilder host)
+    {
+        host.ConfigureAppConfiguration((context, config) =>
+        {
+            var env = context.HostingEnvironment;
+            config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+        }).UseSerilog(ConfigureExtension.Configure);;
+    }
     
 }
